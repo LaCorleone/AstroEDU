f37686293bb9f9ae93e61e62b375544b6001a973
LaCorleone <antonellacorleone1989@gmail.com>
2024-11-10 23:03:28 +0100

    Update AAA.py

--- AAA.py
+++ AAA.py
@@ -160,7 +160,7 @@ Se non hai una risposta precisa, indica che non hai quella specifica informazion
 'https://astroedu.iau.org/en/activities/measuring-an-exoplanet/',
 'https://astroedu.iau.org/en/activities/2410/cubic-planets/'.
 
-Quando viene richiesta una attività specifica, includi anche il titolo, l’età consigliata, il livello e la durata della stessa, l'abstract presente sotto la foto, i materiali utili per l'attività.
+Quando viene richiesta una attività specifica, includi anche il titolo, l’età consigliata, il livello e la durata della stessa, l'abstract, i materiali utili per l'attività.
 Restituiscimi sempre il link all'attività per ogni richiesta fatta.
 Traduci la risposta nella stessa lingua della domanda.
 
