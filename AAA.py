2aea3da0edd1ae6fad4e6c5a8d01a2c24c187e6d
LaCorleone <antonellacorleone1989@gmail.com>
2024-11-12 15:25:11 +0100

    Update AAA.py

--- AAA.py
+++ AAA.py
@@ -24,7 +24,7 @@ vectorstore = Chroma(embedding_function=embeddings,persist_directory="./chroma_d
 # Retrieve and generate using the relevant snippets of the blog.
 retriever = vectorstore.as_retriever()
 
-llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0, openai_api_key=openai_api_key)
+llm = ChatOpenAI(model_name="gpt-4o", temperature=0, openai_api_key=openai_api_key)
 
 contextualize_q_system_prompt = """Given a chat history and the latest user question \
 which might reference context in the chat history, formulate a standalone question \
