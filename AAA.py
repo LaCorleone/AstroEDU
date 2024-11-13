7f8bc053134e4fc5d3d1c8c788989f79d1dae39a
LaCorleone <antonellacorleone1989@gmail.com>
2024-11-13 16:27:50 +0100

    Update AAA.py

--- AAA.py
+++ AAA.py
@@ -152,8 +152,8 @@ Di seguito i dettagli per ogni link con i campi da mettere nell'elenco puntato o
 'https://astroedu.iau.org/en/activities/1302/star-in-a-box-high-school/':12-19,Middle School Secondary, 45 minutes, Computer with internet access, Star in a Box worksheets.
 'https://astroedu.iau.org/en/activities/1304/model-of-a-black-hole/':8-12, Primary, 1 hour, Light elastic bandage (e.g., Tubifix), Small marble, Heavy ball (e.g., boules, bocce, or pétanque ball). 
 'https://astroedu.iau.org/en/activities/1303/design-your-alien/':8-12, Primary, 2 hours, Crayons, Paper, Printed copy of Design Your Alien script (optional), Design Your Alien presentation (optional)
-Quando mi dai come risposta uno di questi link mettimi sempre un riassunto della risorsa didattica in questione dopo l'elenco puntato nella stessa lingua della domanda. 
-Se mi dai come risposta più di un link allora metti il riassunto per ogni link e l'elenco puntato con tutte le informazioni indicate sopra e ricorda di utilizzare la stessa lingua della domanda per generare la risposta.
+Quando mi dai come risposta uno di questi link mettimi sempre un riassunto della risorsa didattica in questione dopo l'elenco puntato.
+Se mi dai come risposta più di un link allora metti il riassunto per ogni link e l'elenco puntato con tutte le informazioni indicate sopra.
 
 Questi quattro link 
 'https://astroedu.iau.org/en/activities/meet-our-home-planet-earth/',
