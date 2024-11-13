80034817f95979d82e95fdeefabc20b4c586c29a
LaCorleone <antonellacorleone1989@gmail.com>
2024-11-13 22:04:51 +0100

    Update AAA.py

--- AAA.py
+++ AAA.py
@@ -42,12 +42,11 @@ contextualize_q_prompt = ChatPromptTemplate.from_messages(
 history_aware_retriever = create_history_aware_retriever(llm, retriever, contextualize_q_prompt)
 
 qa_system_prompt = """
-Comportati come un esperto in didattica.
-
+Comportati come un esperto in didattica. 
 Se la domanda è generica per esempio "consigliami qualche attività didattica da fare" oppure "cerco qualcosa" e frasi simili a queste e ti viene chiesto qualcosa senza specificare l'argomento da trattare, allora chiedimi l'argomento, l'età e la durata di quello che sto richiedendo.
-Nella risposta includi sempre il link che hai a disposizione associato alla risorsa didattica e mettimi tutto in un elenco puntato nel seguenti campi: link, età, livello,durata, materiali.
-
-Di seguito i dettagli in inglese per ogni link con i campi da mettere nell'elenco puntato ossia link, età, livello, durata, materiali:
+Rileva la lingua che viene utilizzata nelle domande ed utilizza la stessa lingua per rispondermi.
+Utilizza solo le informazioni che hai per rispondere alle domande e se non hai la risposta dimmi che non lo sai.
+Dammi sempre il link che hai a disposizione associato alla risorsa didattica:
 'https://astroedu.iau.org/en/activities/2403/find-the-hidden-rainbows/',
 'https://astroedu.iau.org/en/activities/2406/discover-earths-climate-with-a-balloon/',
 'https://astroedu.iau.org/en/activities/2405/the-gravity-battle/',
