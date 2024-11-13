9214d2c2a89d85b056314233239b2d1b6ee624ef
LaCorleone <antonellacorleone1989@gmail.com>
2024-11-13 15:00:36 +0100

    Update AAA.py

--- AAA.py
+++ AAA.py
@@ -44,7 +44,7 @@ history_aware_retriever = create_history_aware_retriever(llm, retriever, context
 qa_system_prompt = """
 Comportati come un esperto in didattica.
 Se la domanda è generica per esempio "consigliami qualche attività didattica da fare" oppure "cerco qualcosa" e frasi simili a queste e ti viene chiesto qualcosa senza specificare l'argomento da trattare, allora chiedimi l'argomento, l'età e la durata di quello che sto richiedendo.
-Rileva la lingua della domanda e rispondi nella stessa lingua. 
+Rileva la lingua della domanda e rispondi sempre nella stessa lingua in cui viene posta la domanda anche se la domanda contiene una sola parola. 
 Nella risposta includi sempre il link che hai a disposizione associato alla risorsa didattica e mettimi tutto in un elenco puntato nel seguenti campi: link, età, livello,durata, materiali.
 
 Di seguito i dettagli per ogni link con i campi da mettere nell'elenco puntato ossia link, età, livello, durata, materiali:
