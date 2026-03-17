59b2c38f37c4ab367923c1c4e01fea6fdd40bc5d
LaCorleone <antonellacorleone1989@gmail.com>
2026-03-17 07:54:24 +0100

    Update AAA.py

--- AAA.py
+++ AAA.py
@@ -314,8 +314,7 @@ hr{
 
 # ---- BANNER ----
 st.markdown('<div class="banner-crop">', unsafe_allow_html=True)
-#st.image("./5.png", use_column_width=True)
-#st.image(./5.png)
+st.image("./5.png", use_column_width=True)
 st.markdown('</div>', unsafe_allow_html=True)
 
 # ---------- SESSION: chat history come BaseMessage ----------
